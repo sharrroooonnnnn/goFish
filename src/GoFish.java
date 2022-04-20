2e5a7c7a6bf6da597f6c3a4b8c180c8090d2bc50
sharrroooonnnnn <88515352+sharrroooonnnnn@users.noreply.github.com>
2022-04-19 19:08:23 -0500

    Update GoFish.java

--- src/GoFish.java
+++ src/GoFish.java
@@ -1,114 +1,106 @@
 import java.util.Scanner;
 
-public class GoFish
-{
+public class GoFish {
     public static Scanner input = new Scanner(System.in);
+    public GoFish(){}
 
-    public static void main(String[]args)
-    {
-        boolean repProgram = true;
-        int menu;
-        do
-        {
-            System.out.println("\n1) Test Deck Hand Class\n 2) Play Go Fish\n 3) Exit");
-            menu = input.nextInt();
-            switch(menu)
-            {
+    public static void main(String[] args) {
+        boolean running = true;
+
+        do {
+            System.out.println("\n\n\n [1] Test Deck Hand Class\n" +
+                    " [2] Play Go Fish\n[3] Exit");
+            int menu = input.nextInt();
+            switch (menu) {
                 case 1:
-                    testDecks();
+                    testDeck();
                     break;
                 case 2:
-                    GoFishHand game = new GoFishHand();
+                    new GoFishHand();
                     break;
                 case 3:
-                    repProgram = false;
-                    break;
+                    running = false;
             }
+        } while (running);
 
-        }while(repProgram);
-        System.out.println("Exiting!");
+        System.out.println("Goodbye!");
     }
 
+    public static void testDeck() {
+        boolean runningF = true;
 
-    public static void testDecks()
-    {
-        boolean repTest = true;
-        int hand;
-        Deck fullDeck;
-        Deck emptyHand;
-        do
-        {
+        do {
             System.out.println("Which Deck would you like to manipulate?\n" +
-                    "[1] Empty Hand\n[2] Full Deck\nEnter the Corresponding number");
-            hand = input.nextInt();
-            switch(hand)
-            {
+                    " [1] Empty Hand\n " +
+                    " [2] Full Deck\n" +
+                    "Enter the Corresponding number");
+            int hand = input.nextInt();
+            switch (hand) {
                 case 1:
-                    emptyHand = new Deck();
-                    repTest = testDeck(emptyHand);
+                    Deck emptyHand = new Deck();
+                    runningF = testDeck(emptyHand);
                     break;
                 case 2:
-                    fullDeck = new Deck();
+                    Deck fullDeck = new Deck();
                     fullDeck.fillDeck();
-                    repTest = testDeck(fullDeck);
-                    break;
+                    runningF = testDeck(fullDeck);
             }
-        }while(repTest);
+        } while (runningF);
+
+
     }
 
-    public static boolean testDeck(Deck deck)
-    {
-        Card temp;
-        int menu;
+    public static boolean testDeck(Deck deck) {
         boolean repDeckTest = true;
-        do
-        {
-            System.out.println("[1] Insert Card\n[2] Delete Card Value\n[3] Delete Random Card\n" +
-                    "[4] Test number times a specific value Occurs in Hand\n" +
-                    "[5] Print Total number of Cards in Deck\n" +
-                    "[6] Display Entire Deck hand\n" +
-                    "[7] Change/Restart Decks\n[8] Exit Test");
-            menu = input.nextInt();
-            switch(menu)
-            {
+
+        do {
+            System.out.println(" [1] Insert card\n" +
+                    " [2] Delete card \n " +
+                    " [3] Delete random card\n " +
+                    " [4] Test number times a specific value happens\n " +
+                    " [5] Print Total number of Cards in Deck\n " +
+                    " [6] Display Entire Deck hand\n " +
+                    " [7] Change/Restart Decks\n" +
+                    " [8] Exit Test");
+            int menu = input.nextInt();
+            switch (menu) {
                 case 1:
-                    int suit, value;
-                    System.out.println("Please enter the Card ID you'd like to insert\n" +
-                            "Both Value and Suit are represented by Integers\nSuits:\n" +
-                            "\t1 = Clubs\n\t2 = Diamonds\n\t3 = Hearts\n\t4 = Spades\n" +
-                            "Enter the Integer Corresponding to the Suit");
-                    suit = input.nextInt();
-                    System.out.println("Okay, now enter the Value for the Card as an integer.");
-                    value = input.nextInt();
+                    System.out.println(" Please enter the Card ID you would like to insert \n"+
+                            " Both Value and Suit are represented by Integers\n" +
+                            "\tSuits:\n\t1 = Clubs\n\t2 = Diamonds\n\t3 = Hearts\n\t4 = Spades\nEnter the Integer Corresponding to the Suit");
+                    int suit = input.nextInt();
+                    System.out.println("Enter the Value for the Card: ");
+                    int value = input.nextInt();
                     Card insert = new Card(value, suit);
                     deck.insertCard(insert);
-                    System.out.println("Card inserted");
+                    System.out.println("Card inserted!");
                     break;
                 case 2:
-                    int deleteVal;
-                    System.out.println("Which card Value would you like to search and Delete?\n" +
-                            "Enter the integer Representation of the Value");
-                    deleteVal = input.nextInt();
-                    Card deleteCard;
-                    deleteCard = deck.deleteValue(deleteVal);
-                    if(deleteCard == null)
+                    System.out.println("Which card Value would you like to Delete?\n" +
+                            "Enter the Value: ");
+                    int deleteVal = input.nextInt();
+                    Card deleteCard = deck.deleteValue(deleteVal);
+
+                    if (deleteCard == null) {
                         System.out.println("Card not found.");
-                    else
+                    } else {
                         System.out.println("Card Deleted: " + deleteCard);
+                    }
                     break;
                 case 3:
-                    Card randomDelete;
-                    randomDelete = deck.deleteAnyCard();
-                    if(randomDelete == null)
-                        System.out.println("Empty Hand");
-                    else
+                    Card randomDelete = deck.deleteAnyCard();
+
+                    if (randomDelete == null) {
+                        System.out.println("You don't have anything on your hand");
+                    } else {
                         System.out.println("Card deleted : " + randomDelete);
+                    }
                     break;
                 case 4:
                     System.out.println("Which value would you like Search for?");
                     int searchVal = input.nextInt();
-                    System.out.println("That value occurs " + deck.getCount(searchVal) +
-                            " times in the Deck Hand");
+                    int searchValue = deck.getCount(searchVal);
+                    System.out.println("That value occurs " + searchValue + " times in the Deck Hand");
                     break;
                 case 5:
                     System.out.println("Total number of Cards in Deck : " + deck.getSize());
@@ -123,7 +115,8 @@ public class GoFish
                     repDeckTest = false;
                     return false;
             }
-        }while(repDeckTest);
+        } while (repDeckTest);
+
         return true;
     }
 }
